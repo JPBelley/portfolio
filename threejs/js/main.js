176247e46ea1110855c3dcc3ed18e47254bcaeb7
Unknown <belleyjeanphilippe@gmail.com>
2017-11-20 18:54:32 -0500

    Update

--- threejs/js/main.js
+++ threejs/js/main.js
@@ -2,15 +2,26 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 var mouseX = 0, mouseY = 0;
 
+// Rendering the scene
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// Resizing the window
+window.addEventListener('resize', function() {
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.updateProjectionMatrix();
+});
+
+// Creating the cube
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF } );
+var material = new THREE.MeshNormalMaterial( { color: 0xFFFFFF } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+// Position the camera to see the cube
 camera.position.z = 5;
 var controls = new THREE.OrbitControls( camera );
 
